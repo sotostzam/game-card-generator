1f983b5a62cf3c6ab380469d1b7e18f0b3d55b2f
Sotiris Tzamaras <sotiris@tzamaras.com>
2024-03-04 22:33:34 +0200

    add accent coloring and icons

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 import math
 import json
+import textwrap
 
 class Card:
 	"""
 	Class representing a customizable game card.
 	"""
-	def __init__(self, name, type, info, effect, image, attributes=None, border_size=5, font_size=30, font_path=None):
+	def __init__(self, name, type, description, effect=None, attributes=None, image=None):
 		"""
 		Initializes a Card object.
 
 		Args:
 		name: (str) Name of the card.
-		info: (str) Additional information for the card.
-		image_path: (str) Path to the image for the card.
-		border_size: (int, optional) Size of the card border (default: 10).
-		font_size: (int, optional) Size of the text font (default: 20).
-		font_path: (str, optional) Path to a custom font file (default: None).
+		type: (str) Type of the card.
+		description: (str) Description for the card.
+		effect: (str) Path to the image for the card.
+		image: (str) Path to the image for the card.
 		"""
 		self.name = name
 		self.type = type
-		self.info = info
+		self.description = description
 		self.effect = effect
-		self.image_path = image
-		self.border_size = border_size
+		self.image = image
+		self.border_size = 5
 		self.attributes = attributes
 
-		self.font_size = font_size
-		self.font_path = font_path
-		self.normal_font = ImageFont.truetype(f"fonts/{self.font_path}" or "arial.ttf", self.font_size)
+		self.font_size = 30
+		self.normal_font = ImageFont.truetype("fonts/EduSABeginner.ttf" or "arial.ttf", self.font_size)
+		self.title_font = ImageFont.truetype("fonts/asap_symbol.otf" or "arial.ttf", self.font_size+10)
+		self.type_font = ImageFont.truetype("fonts/EduSABeginner_bold.ttf" or "arial.ttf", self.font_size-10)
 
 		self.layout_size = (750, 1050)
 
 		self.inner_bg = "black"
+		self.red = ImageColor.getrgb("#bf3f3f")
 		self.green = ImageColor.getrgb("#9ac360")
+		self.blue = ImageColor.getrgb("#4446c2")
 		self.yellow = ImageColor.getrgb("#966a00")
+
+		# Set the card accent colors and class based on creature type
+		if self.type == 'normal':
+			self.accent_color = self.blue
+			self.card_class = 'creature'
+		elif self.type == 'annoying':
+			self.accent_color = self.red
+			self.card_class = 'creature'
+		elif self.type == 'mythical':
+			self.accent_color = self.yellow
+			self.card_class = 'creature'
 
 	def makeRectangle(self, l, w, theta, offset=(0,0)):
 		c, s = math.cos(theta), math.sin(theta)
@@ -57,7 +71,7 @@ class Card:
 		card.paste(layout, (self.border_size, self.border_size))
 
 		# Put the card's image
-		card_avatar = Image.open(f"images/{self.image_path}")
+		card_avatar = Image.open(f"card_images/{self.image}")
 		card.paste(card_avatar, ((card_width - card_avatar.size[0]) // 2, int(card_height * 0.6 - card_avatar.size[1]) // 2))
 
 		# Initialize drawing methods
@@ -68,7 +82,7 @@ class Card:
 
 		# Draw the layout's shapes
 		shape = [(0 + self.border_size, card_height - (card_height * .377)), (card_width - self.border_size - 0.01, card_height - (card_height * .377))]
-		draw.line(shape, fill=self.green, width=20)
+		draw.line(shape, fill=self.accent_color, width=20)
 
 		circle_radius = 65
 		circle_x = (details_pad_left - circle_radius * 2) // 2
@@ -76,24 +90,40 @@ class Card:
 
 		draw.ellipse((circle_x, circle_y, circle_x + circle_radius * 2, circle_y + circle_radius * 2),
 					  fill=self.inner_bg,
-					  outline=self.green,
+					  outline=self.accent_color,
 					  width=10)
+		
+		card_class_image = Image.open(f"assets/{self.card_class}.png")
+		card_class_image = card_class_image.resize([int(0.15 * s) for s in card_class_image.size], Image.LANCZOS)
+
+		card_class_image_x = int(details_pad_left - card_class_image.size[0]) // 2
+		card_class_image_y = int(card_height - (card_height * .417))
+		card.paste(card_class_image, (card_class_image_x, card_class_image_y), mask=card_class_image)
 
 		vertices = self.makeRectangle(41, 41, 45*math.pi/180, offset=(card_width * .308, card_height - (card_height * .377)))
-		draw.polygon(vertices, fill=self.inner_bg, width=10, outline=self.green)
+		draw.polygon(vertices, fill=self.inner_bg, width=10, outline=self.accent_color)
 
 		# Draw the circles for the attributes
 		if self.attributes:
 			circle_radius = 35
 			attributes_pad_left = (details_pad_left - circle_radius * 2) // 2
-			attributes_pad_top = card_height - (card_height * .28)
+			attributes_pad_top = int(card_height - (card_height * .28))
 
 			for attribute, value in self.attributes.items():
 				draw.ellipse((attributes_pad_left, attributes_pad_top,
 							attributes_pad_left + circle_radius * 2, attributes_pad_top + circle_radius * 2),
 							fill=self.inner_bg,
-							outline=self.yellow,
+							outline=self.accent_color,
 					  		width=5)
+				
+				try:
+					attribute_img = Image.open(f"assets/{value}.png")
+					attribute_img = attribute_img.resize([int(0.08 * s) for s in attribute_img.size], Image.LANCZOS)
+					attribute_img_x = int(details_pad_left - attribute_img.size[0]) // 2
+					attribute_img_y = attributes_pad_top + 15
+					card.paste(attribute_img, (attribute_img_x, attribute_img_y), mask=attribute_img)
+				except Exception:
+					pass
 				
 				attributes_pad_top += 100
 
@@ -101,23 +131,23 @@ class Card:
 		current_padding = details_pad_top
 
 		# Set Title
-		title_font = ImageFont.truetype("fonts/asap_symbol.otf" or "arial.ttf", self.font_size+10)
-		draw.text((details_pad_left, current_padding), self.name.upper(), fill="black", font=title_font)
-		current_padding += title_font.getbbox(self.name.upper())[3] + 5
+		draw.text((details_pad_left, current_padding), self.name.upper(), fill="black", font=self.title_font)
+		current_padding += self.title_font.getbbox(self.name.upper())[3] + 5
 
 		# Set Type
-		type_font = ImageFont.truetype("fonts/EduSABeginner_bold.ttf" or "arial.ttf", self.font_size-10)
-		draw.text((details_pad_left, current_padding), self.type, fill=ImageColor.getrgb("#966a00"), font=type_font)
-		current_padding += type_font.getbbox(self.type)[3] + 10
+		draw.text((details_pad_left, current_padding), f"{self.type.capitalize()} Encounter", fill=self.accent_color, font=self.type_font)
+			
+		current_padding += self.type_font.getbbox(self.type)[3] + 10
 
 		# Information
-		draw.text((details_pad_left, current_padding), self.info, fill="black", font=self.normal_font)
-		current_padding += self.normal_font.getbbox(self.info)[3] + 10
+		para = textwrap.wrap(self.description, width=42)
+		for line in para:
+			draw.text((details_pad_left, current_padding), line, fill="black", font=self.normal_font)
+			current_padding += self.font_size + 5
+		current_padding += 12
 
 		# Effect
-		import textwrap
-		para = textwrap.wrap(f"Effect: {self.effect}", width=40)
-		
+		para = textwrap.wrap(f"Effect: {self.effect}", width=42)
 		for line in para:
 			draw.text((details_pad_left, current_padding), line, fill="black", font=self.normal_font)
 			current_padding += self.font_size + 5
